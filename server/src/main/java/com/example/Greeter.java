40a0f3a54d663c43eebe4c304ed5a4a0bd5f18fb
qinqin75 <52749880+qinqin75@users.noreply.github.com>
2019-10-30 11:15:38 +0100

    Update Greeter.java
    correction

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -1,15 +1,21 @@
 package com.example;
 
+/**
+*@author stagiaire
+*/
 public class Greeter {  
   public Greeter() {
-  }
-/**
-*@stagiaire
-*/
-  public final String greet(final String someone) {
     /**
-    *@return
+    *default constructeur.
     */
+  }
+
+    /**
+    *@param  someone someone
+    *@return String
+    */
+  public final String greet(final String someone) {
+
     return String.format("Hello babacar, %s!", someone);
   }
 }
