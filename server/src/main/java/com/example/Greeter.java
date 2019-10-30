c7ae477f0bccf59f50e860bd3d41633b36c1bdc1
qinqin75 <52749880+qinqin75@users.noreply.github.com>
2019-10-30 11:01:45 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -4,7 +4,9 @@ public class Greeter {
   
   public Greeter() {
   }
-
+/**
+*@stagiaire
+*/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
